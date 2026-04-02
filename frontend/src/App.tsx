cac9b77d4d0a2a0d97b9503419bc3b779a757cf7
Rafael Sette de Araujo <138353834+Sette0o0@users.noreply.github.com>
2026-04-02 10:55:26 -0300

    refactor (#88, #32, #33): integração com micro serviço e informações da tarefa

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,15 @@
-import { BrowserRouter, Routes, Route, Navigate, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import AppRoutes from './routes/AppRoutes'
 import { AuthProvider } from './contexts/AuthContext'
-
-import AppLayout from "./components/layout/AppLayout";
-import Dashboard from "./components/pages/Dashboard";
-import Usuarios from "./components/pages/Usuarios";
-import Projetos from "./components/pages/Projetos";
+import { ToastContainer } from "react-toastify";
 
 export default function App() {
   return (
     <AuthProvider>
       <RouterProvider router={AppRoutes} />
+      <ToastContainer
+        theme="dark"
+      />
     </AuthProvider>
   )
 }
